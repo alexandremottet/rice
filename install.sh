c8867a2d8af56cea30e7d657c611462eb19271b5
Alexandre MOTTET <mottetalexandre@gmail.com>
2021-03-09 23:20:16 +0100

    add i3

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ sudo apt-get -y upgrade;
 
 sudo apt-get install -y build-essential git vim xcb;
 
+# i3
+sudo apt install -y i3
+
 # Polybar
-sudo apt install polybar;
+sudo apt install -y polybar;
 chmod +x $HOME/.config/polybar/launch.sh
 cp /usr/share/doc/polybar/config .config/polybar/config
