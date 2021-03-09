1ae4e7ce07916b26e63f5eea6a08022af185893c
Alexandre MOTTET <mottetalexandre@gmail.com>
2021-03-09 23:05:02 +0100

    Update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar
 
 echo ¨---¨ |  tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polyvar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo ¨Bars launched...¨
--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ sudo apt-get -y update;
 sudo apt-get -y upgrade;
 
 sudo apt-get install -y build-essential git vim xcb;
+
+# Polybar
 sudo apt install polybar;
-
 chmod +x $HOME/.config/polybar/launch.sh
-
+cp /usr/share/doc/polybar/config .config/polybar/config
