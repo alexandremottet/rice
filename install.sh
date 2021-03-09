4e7daa3b64f1c811c788b137a2f3a19cc51b453e
Alexandre MOTTET <mottetalexandre@gmail.com>
2021-03-10 00:00:04 +0100

    update polybar install + bspwm

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@ sudo apt-get -y upgrade;
 
 sudo apt-get install -y build-essential git vim xcb;
 
+# xrdb / color manager
+sudo apt install -y xrdb
+
+# bspwm
+sudo apt-get install -y bspwm
+
 # i3
-sudo apt install -y i3
+# sudo apt install -y i3
 
 # Polybar
 sudo apt install -y polybar;
 chmod +x $HOME/.config/polybar/launch.sh
-cp /usr/share/doc/polybar/config .config/polybar/config
 
 # --------------------------------------
 
