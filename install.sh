4632e29b91a92f6988be6d758c8cd5509b61ab8b
Alexandre MOTTET <mottetalexandre@gmail.com>
2021-03-10 01:36:57 +0100

    add nitrogen

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo apt-get -y update;
 sudo apt-get -y upgrade;
 sudo add-apt-repository ppa:aslatter/ppa
 
-sudo apt-get install -y build-essential git vim xcb alacritty;
+sudo apt-get install -y build-essential git vim xcb alacritty nitrogen;
 
 # allreadyinstalled - xrdb / color manager
 # sudo apt-get install -y xrdb
